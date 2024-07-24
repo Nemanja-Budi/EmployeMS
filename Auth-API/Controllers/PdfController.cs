f4b70211828c292edcf956c6d04993d9c7acfa4b
Nemanja Budimirovic <96budi@gmail.com>
2024-07-24 08:41:58 +0200

    Dodavanje pdf kontrolera

---
+++ Auth-API/Controllers/PdfController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+
+namespace ADMitroSremEmploye.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        [HttpPost("generate")]
+        public IActionResult GeneratePdf([FromBody] HtmlContentRequest request)
+        {
+            var htmlContent = request.HtmlContent;
+
+            var pdf = new HtmlToPdf();
+            var doc = pdf.ConvertHtmlString(htmlContent);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                doc.Save(memoryStream);
+                return File(memoryStream.ToArray(), "application/pdf", "employee-salary.pdf");
+            }
+        }
+    }
+
+    public class HtmlContentRequest
+    {
+        public string HtmlContent { get; set; }
+    }
+}
+
