266244874d14dc77622a2b77690ff3f8c17915f0
Nemanja Budimirovic <96budi@gmail.com>
2024-07-30 08:32:34 +0200

    Ispravka na salary kontroleru da vraca prilikom brisanja ispravan odgovor.

--- Auth-API/Controllers/EmployeSalaryController.cs
+++ Auth-API/Controllers/EmployeSalaryController.cs
@@ -101,7 +101,8 @@ namespace ADMitroSremEmploye.Controllers
                 return NotFound($"Employee with Id {employeId} not found or no salaries found.");
             }
 
-            return Ok($"Successfully deleted salaries for Employee with Id {employeId}.");
+            //return Ok($"Successfully deleted salaries for Employee with Id {employeId}.");
+            return Ok(new { message = $"Successfully deleted EmployeSalary with Id {employeId}." });
         }
 
         [HttpDelete("delete-employe-salary/{employeSalaryId}")]
@@ -114,7 +115,8 @@ namespace ADMitroSremEmploye.Controllers
                 return NotFound($"EmployeSalary with Id {employeSalaryId} not found.");
             }
 
-            return Ok($"Successfully deleted EmployeSalary with Id {employeSalaryId}.");
+            //return Ok($"Successfully deleted EmployeSalary with Id {employeSalaryId}.");
+            return Ok(new { message = $"Successfully deleted EmployeSalary with Id {employeSalaryId}." });
         }
 
         [HttpPut("update-employe-salary")]
